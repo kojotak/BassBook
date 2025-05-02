133393bf02c05ff7731ca1ae9463f4dffee325f8
Tomas Beneda <Tomas.Beneda@gmail.com>
2025-05-02 22:43:14 +0200

    thymeleaf controller and template

--- src/main/java/com/github/kojotak/bassbook/BassbookApplication.java
+++ src/main/java/com/github/kojotak/bassbook/BassbookApplication.java
@@ -1,24 +1,13 @@
 package com.github.kojotak.bassbook;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BassbookApplication implements CommandLineRunner {
-
-	@Value("${spring.application.name}")
-	private String applicationName;
+public class BassbookApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BassbookApplication.class, args);
 	}
-
-	@Override
-	public void run(String... args) {
-		System.out.println("Loading " + applicationName);
-	}
-
 
 }
---
+++ src/main/java/com/github/kojotak/bassbook/BassbookController.java
@@ -0,0 +1,19 @@
+package com.github.kojotak.bassbook;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BassbookController {
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("applicationName", applicationName);
+        return "index";
+    }
+}
