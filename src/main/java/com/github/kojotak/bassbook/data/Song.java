4ffa4b7980205ee906ec498202b3e7f2aef3b6a6
Tomas Beneda <Tomas.Beneda@gmail.com>
2025-05-19 22:15:48 +0200

    different builders for one or more songs

--- src/main/java/com/github/kojotak/bassbook/data/Song.java
+++ src/main/java/com/github/kojotak/bassbook/data/Song.java
@@ -33,35 +33,29 @@ public record Song (
     }
 
     public static SongBuilder from(Author author){
-        var builder = new SongBuilder();
-        builder.author = author;
-
-        //set defaults
-        return builder.withDefaults();
+        return new SongBuilder().author(author);
     }
 
     public static class SongBuilder {
 
         private Author author;
-        private int index = 0;
-        private List<String> names = new ArrayList<>();
-        private List<Meter> meters = new ArrayList<>();
-        private List<Feel> feels = new ArrayList<>();
-        private final List<List<Youtube>> playList = new ArrayList<>();
+        private String name;
+        private Meter meter = Meter.COMMON;
+        private Feel feel = Feel.STRAIGHT;
+        private final List<Youtube> plays = new ArrayList<>();
 
         public SongBuilder name(String name){
-            names.add(index, name);
+            this.name = name;
             return this;
         }
 
         public SongBuilder author(Author author){
-            Assert.isNull(author, "author can not be reset");
             this.author = author;
             return this;
         }
 
         public SongBuilder meter(Meter meter){
-            meters.add(index, meter);
+            this.meter = meter;
             return this;
         }
 
@@ -70,36 +64,31 @@ public record Song (
         }
 
         public SongBuilder feel(Feel feel){
-            feels.add(index, feel);
+            this.feel = feel;
             return this;
         }
 
         public SongBuilder youtube(Channel channel, String id){
-            playList.get(index).add(new Youtube(channel, id));
+            plays.add(new Youtube(channel, id));
             return this;
         }
 
         public SongBuilder youtube(Channel channel, String id, EnumSet<Technique> technique){
-            playList.get(index).add(new Youtube(channel, id, technique));
+            plays.add(new Youtube(channel, id, technique));
             return this;
         }
 
         public SongBuilder youtube(Channel channel, String id, Tuning tuning){
-            playList.get(index).add(new Youtube(channel, id, tuning, EnumSet.noneOf(Technique.class)));
+            plays.add(new Youtube(channel, id, tuning, EnumSet.noneOf(Technique.class)));
             return this;
         }
 
         public SongBuilder youtube(Channel channel, String id, Tuning tuning, EnumSet<Technique> technique){
-            playList.get(index).add(new Youtube(channel, id, tuning, technique));
+            plays.add(new Youtube(channel, id, tuning, technique));
             return this;
         }
 
         public Song build(){
-            Assert.isTrue(index == 0, "can not build one song from multiple songs");
-            var name = names.getFirst();
-            var feel = feels.getFirst();
-            var meter = meters.getFirst();
-            var plays = playList.getFirst();
             Assert.notEmpty(plays, "plays can not be empty");
             Objects.requireNonNull(name, "name is required");
             Objects.requireNonNull(author, "author is required");
@@ -114,6 +103,76 @@ public record Song (
             );
         }
 
+        public SongsBuilder next(){
+            return new SongsBuilder()
+                    .author(author)
+                    .name(name)
+                    .feel(feel)
+                    .meter(meter)
+                    .youtube(plays)
+                    .next();
+        }
+    }
+
+    public static class SongsBuilder {
+
+        private Author author;
+        private int index = 0;
+        private final List<String> names = new ArrayList<>();
+        private final List<Meter> meters = new ArrayList<>();
+        private final List<Feel> feels = new ArrayList<>();
+        private final List<List<Youtube>> playList = new ArrayList<>();
+
+        public SongsBuilder name(String name){
+            names.add(index, name);
+            return this;
+        }
+
+        public SongsBuilder author(Author author){
+            Assert.isNull(this.author, "author can not be reset");
+            this.author = author;
+            return this;
+        }
+
+        public SongsBuilder meter(Meter meter){
+            meters.add(index, meter);
+            return this;
+        }
+
+        public SongsBuilder meter(int count, int perBeat){
+            return meter(new Meter(count, perBeat));
+        }
+
+        public SongsBuilder feel(Feel feel){
+            feels.add(index, feel);
+            return this;
+        }
+
+        public SongsBuilder youtube(List<Youtube> plays){
+            playList.add(index, plays);
+            return this;
+        }
+
+        public SongsBuilder youtube(Channel channel, String id){
+            playList.get(index).add(new Youtube(channel, id));
+            return this;
+        }
+
+        public SongsBuilder youtube(Channel channel, String id, EnumSet<Technique> technique){
+            playList.get(index).add(new Youtube(channel, id, technique));
+            return this;
+        }
+
+        public SongsBuilder youtube(Channel channel, String id, Tuning tuning){
+            playList.get(index).add(new Youtube(channel, id, tuning, EnumSet.noneOf(Technique.class)));
+            return this;
+        }
+
+        public SongsBuilder youtube(Channel channel, String id, Tuning tuning, EnumSet<Technique> technique){
+            playList.get(index).add(new Youtube(channel, id, tuning, technique));
+            return this;
+        }
+
         public List<Song> buildAll(){
             return IntStream.range(0, index+1).mapToObj(
                     i -> new Song(
@@ -126,19 +185,19 @@ public record Song (
             ).toList();
         }
 
-        public SongBuilder next(){
+        public SongsBuilder next(){
             index++;
             return withDefaults();
         }
 
-        private List<Youtube> sorted(List<Youtube> plays){
-            return plays.stream().sorted(Comparator.comparing(p -> p.channel().label)).toList();
-        }
-
-        private SongBuilder withDefaults(){
+        private SongsBuilder withDefaults(){
             this.playList.add(index, new ArrayList<>());
             return meter(Meter.COMMON).feel(Feel.STRAIGHT);
         }
+    }
+
+    private static List<Youtube> sorted(List<Youtube> plays){
+        return plays.stream().sorted(Comparator.comparing(p -> p.channel().label)).toList();
     }
 
 }
