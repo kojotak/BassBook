e3ecc2044c44e167a2423dffe0bb29f40f6dd9b5
Tomas Beneda <Tomas.Beneda@gmail.com>
2025-11-09 22:28:26 +0100

    added Hey Jude

--- src/main/java/com/github/kojotak/bassbook/BassbookDatabase.java
+++ src/main/java/com/github/kojotak/bassbook/BassbookDatabase.java
@@ -241,7 +241,8 @@ public class BassbookDatabase implements ApplicationListener<ContextRefreshedEve
         return Song.from(Author.THE_BEATLES)
                     .name("Lady Madonna").youtube(BRAND73, "YxVCt81YNOE").next()
                     .name("Something").youtube(NOCCO_CAT, "8YniwKLqZEc").next()
-                    .name("Norwegian Wood").meter(6,8).youtube(TOM_BORNEMANN, "ra9qUqa2UWI")
+                    .name("Norwegian Wood").meter(6,8).youtube(TOM_BORNEMANN, "ra9qUqa2UWI").next()
+                    .name("Hey Jude").youtube(NOCCO_CAT, "89ElXR60JR0")
                 .buildAll();
     }
 
